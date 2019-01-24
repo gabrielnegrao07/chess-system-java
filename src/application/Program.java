cd1330aeceb66c4925e0a8dd069651421be4f7f7
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-01-24 08:15:28 -0200

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
+
+        Position pos = new Position(2, 7);
+        System.out.println(pos);
         
     }
 }
